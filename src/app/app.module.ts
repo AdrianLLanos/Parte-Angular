2734ab7c994dccd1093e2e9bc683d12b1db4cc13
Adrian Alejandro LLanos Zenteno <73763617+AdrianLLanos@users.noreply.github.com>
2021-10-24 22:43:54 -0400

    primer  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MenuprincipalComponent } from './menu/menuprincipal/menuprincipal.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuprincipalComponent
   ],
   imports: [
     BrowserModule
